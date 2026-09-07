6a25f7627d9edbe030f61155f1978da04a41f11b
MiNi-AI-DEV <306247087+MiNi-AI-DEV@users.noreply.github.com>
2026-09-07 16:45:39 +0530

    updated student.java

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String name;
+    private int age;
+    private String qualification;
+    private double percentage;
+    private String contactNo;
+    private String registrationId;
+    private String password; 
+}
