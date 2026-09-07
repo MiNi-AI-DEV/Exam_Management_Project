4b96354e3a38f57d78b8e7ffdcc51f6cd3b33d46
MiNi-AI-DEV <306247087+MiNi-AI-DEV@users.noreply.github.com>
2026-09-07 16:08:30 +0530

    updated main.java

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class main {
-    public static void main (strings[] args) {
-        
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     EXAM MANAGEMENT SYSTEM");
+        System.out.println("=================================");
+
     }
 }
