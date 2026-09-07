c9efb936cd5c8edc45be06deba480e3fbced1bf4
MiNi-AI-DEV <306247087+MiNi-AI-DEV@users.noreply.github.com>
2026-09-07 17:23:44 +0530

    create student constructor

--- Student.java
+++ Student.java
@@ -1,4 +1,5 @@
-public class Student {
+public class Student 
+{
     private String name;
     private int age;
     private String qualification;
@@ -6,4 +7,14 @@ public class Student {
     private String contactNo;
     private String registrationId;
     private String password; 
+public Student(String name, int age,String qualification,double percentage, String contactNo, String registrationId, String password) 
+{
+    this.name = name;
+    this.age = age;
+    this.qualification = qualification;
+    this.percentage = percentage;
+    this.contactNo = contactNo;
+    this.registrationId = registrationId;
+    this.password = password;
+}
 }
