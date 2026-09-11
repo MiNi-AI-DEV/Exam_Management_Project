b6266c794b1714371adfe48c3f18e5ea7ac91811
MiNi-AI-DEV <306247087+MiNi-AI-DEV@users.noreply.github.com>
2026-09-11 18:00:17 +0530

    create questio.njava file

---
+++ Question.java
@@ -0,0 +1,17 @@
+public class Question {
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String correctAnswer;
+
+    public Question(String questionText, String optionA, String optionB, String optionC, String optionD, String correctAnswer) {
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.correctAnswer = correctAnswer;
+    }  
+}
